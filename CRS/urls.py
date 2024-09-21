8c45926fc3194758c0475f0356b7f78593609845
bgumel <sadiqaliyugumel@gmail.com>
2024-09-21 14:34:38 +0100

    Removed unsed import

--- CRS/urls.py
+++ CRS/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from ninja import NinjaAPI, Redoc
+from ninja import NinjaAPI
 from ITN.api import router as itn_router
 
 api = NinjaAPI(title="ITN Distribution API",
